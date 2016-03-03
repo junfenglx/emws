9ab7503da289d22c86404039a6fe2a641c8de71d
junfeng_hu <junfenglx@gmail.com>
2016-03-03 16:45:02 +0800

    done port script2.py to C++

--- emws-cpp/src/emws-train.cpp
+++ emws-cpp/src/emws-train.cpp
@@ -3,13 +3,18 @@
 //
 
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <cstdio>
+#include <ctime>
+#include <iomanip>
 
 #include <boost/program_options.hpp>
+#include <rapidjson/prettywriter.h>
 
 #include "rapidjson/document.h"
 #include "rapidjson/filereadstream.h"
+#include "rapidjson/stringbuffer.h"
 
 #define ELPP_DEFAULT_LOG_FILE "logs/emws-train-%datetime.log"
 
@@ -20,6 +25,37 @@ INITIALIZE_EASYLOGGINGPP
 
 #include "base_seger.h"
 
+void print_config(rapidjson::Document const &config) {
+
+    using namespace std;
+    for (auto m = config.MemberBegin();
+         m != config.MemberEnd(); ++m) {
+        cout << '\t' << m->name.GetString() << ": ";
+        if (m->value.IsString())
+            cout << m->value.GetString();
+        else if (m->value.IsInt())
+            cout << m->value.GetInt();
+        else if (m->value.IsDouble())
+            cout << m->value.GetDouble();
+        else if (m->value.IsBool())
+            cout << (m->value.GetBool() ? "true" : "false");
+        else if (m->value.IsNull())
+            cout << "null";
+        else
+            cout << "unknown type";
+        cout << endl;
+    }
+}
+
+const std::string gen_model_path(std::string const &model_name, std::string const &base_dir=".") {
+    using namespace std;
+    time_t t = time(nullptr);
+    tm t_m = *localtime(&t);
+    auto const now_str = put_time(&t_m, "%F_%H-%M");
+    std::ostringstream ate(std::ios_base::ate);
+    ate << base_dir << '/' << model_name << '_' << now_str << ".model";
+    return ate.str();
+}
 
 int main(int argc, const char *argv[]) {
     using namespace std;
@@ -27,11 +63,9 @@ int main(int argc, const char *argv[]) {
     using namespace boost::program_options;
 
     START_EASYLOGGINGPP(argc, argv);
-    LOG(INFO) << "My first info log using default logger";
-    CLOG(ERROR, "performance") << "This is info log using performance logger";
     el::Logger* mainLogger = el::Loggers::getLogger("main");
-    mainLogger->debug("Hello, main logger");
-    CLOG(ERROR, "main") << "CLOG using main logger";
+    // mainLogger->debug("Hello, main logger");
+    // CLOG(ERROR, "main") << "CLOG using main logger";
 
     try
     {
@@ -55,7 +89,9 @@ int main(int argc, const char *argv[]) {
         }
 
         string const filename = vm["config"].as<string>();
-        cout << filename << endl;
+        mainLogger->info("use config file: %v", filename);
+
+        mainLogger->info("parse parameters ...");
         FILE* fp = fopen(filename.data(), "rb");
         char readBuffer[65536];
         FileReadStream is(fp, readBuffer, sizeof(readBuffer));
@@ -63,25 +99,25 @@ int main(int argc, const char *argv[]) {
         config.ParseStream(is);
         fclose(fp);
 
-        for (auto m = config.MemberBegin();
-             m != config.MemberEnd(); ++m) {
-            cout << m->name.GetString() << ": ";
-            if (m->value.IsString())
-                cout << m->value.GetString();
-            else if (m->value.IsInt())
-                cout << m->value.GetInt();
-            else if (m->value.IsDouble())
-                cout << m->value.GetDouble();
-            else if (m->value.IsBool())
-                cout << (m->value.GetBool() ? "true" : "false");
-            else if (m->value.IsNull())
-                cout << "null";
-            else
-                cout << "unknown type";
-            cout << endl;
-        }
-        base_seger *seger = base_seger::create_seger(config, string("emws"));
+        StringBuffer buffer;
+        PrettyWriter<StringBuffer> writer(buffer);
+        config.Accept(writer);
+
+        // print_config(config);
+        // cout << boolalpha << config.IsNull() << endl;
+        mainLogger->info("=====parameters====\n%v", buffer.GetString());
+
+        mainLogger->info("initial model ...");
+        string const model_name = "emws";
+        base_seger *seger = base_seger::create_seger(config, model_name);
+
+        mainLogger->info("train ...");
         seger->train();
+
+        mainLogger->info("save model to %v", gen_model_path(model_name));
+        // TODO save model
+
+        mainLogger->info("train done");
 
         delete seger;
     }

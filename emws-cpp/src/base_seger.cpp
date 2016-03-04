07198501c67101d220db25f5922f568358bc3cad
junfeng_hu <junfenglx@gmail.com>
2016-03-04 10:54:12 +0800

    some changes of base_seger interface

--- emws-cpp/src/base_seger.cpp
+++ emws-cpp/src/base_seger.cpp
@@ -2,6 +2,8 @@
 // Created by junfeng on 3/2/16.
 //
 
+#include <vector>
+
 #include "base_seger.h"
 #include "emws_seger.h"
 
@@ -16,4 +18,28 @@ base_seger * base_seger::create_seger(rapidjson::Document const &config, std::st
         throw msg;
     }
     return seger;
+}
+
+base_seger *base_seger::load(std::string const &model_path, std::string const &seger_name) {
+    base_seger *seger;
+    if (seger_name == "emws") {
+        seger =  new emws_seger(model_path);
+    }
+    else {
+        std::string msg{"unknown seger name: "};
+        msg += seger_name;
+        throw msg;
+    }
+    return seger;
+}
+
+std::vector<std::vector<std::u32string>> base_seger::predict(std::vector<std::u32string> const &sentences) {
+    using namespace std;
+
+    auto segmented_sentences = vector<vector<u32string>>();
+    for (auto const &sentence : sentences) {
+        vector<u32string> segmented_sentence = predict(sentence);
+        segmented_sentences.push_back(segmented_sentence);
+    }
+    return segmented_sentences;
 }
--- emws-cpp/src/base_seger.h
+++ emws-cpp/src/base_seger.h
@@ -16,13 +16,16 @@ class base_seger {
 public:
     virtual void train() = 0;
 
-    virtual void predict(std::vector<std::string> const &corpus) const = 0;
+    std::vector<std::vector<std::u32string>> predict(std::vector<std::u32string> const &sentences);
 
-    virtual void predict(std::string const &paragraph) const = 0;
+    virtual std::vector<std::u32string> predict(std::u32string const &sentence) const = 0;
+
+    virtual bool save(std::string const &model_path) const = 0;
 
     virtual ~base_seger() {}
 
     static base_seger * create_seger(rapidjson::Document const &config, std::string const &seger_name);
+    static base_seger * load(std::string const &model_path, std::string const &seger_name);
 
 };
 

0a97b48d4a4d86c55db8f3d3a8508fda22435847
junfeng_hu <junfenglx@gmail.com>
2016-03-09 16:17:17 +0800

    done predict_single_position

--- emws-cpp/some_test/test_armadillo_example.cpp
+++ emws-cpp/some_test/test_armadillo_example.cpp
@@ -14,5 +14,17 @@ int main()
     mat B = randu<mat>(4,5);
     cout << A*B.t() << endl;
 
+    uvec v;
+    v << 1;
+    // 1 element
+    // override previous value, size not change
+    v << 2;
+    cout << v << endl;
+    uvec v2;
+    // size change, 2 elements
+    v2 << 1 << 2;
+    // re initialize
+    v2 << 3;
+    cout << v2 << endl;
     return 0;
 }
